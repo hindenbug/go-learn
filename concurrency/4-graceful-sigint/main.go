69467617ff2adbd09e4e3958ec04554d6eae6bc3
hindenbug <304184+hindenbug@users.noreply.github.com>
2020-12-26 00:49:07 +0100

    Add solutiion to  graceful-sigint using os.signals channel

--- concurrency/4-graceful-sigint/main.go
+++ concurrency/4-graceful-sigint/main.go
@@ -13,9 +13,22 @@
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
 	// Create a process
 	proc := MockProcess{}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		<-c
+		signal.Reset()
+		proc.Stop()
+	}()
 
 	// Run the process (blocking)
 	proc.Run()
